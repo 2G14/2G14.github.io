cd6e6ad47156a35f5651683b727b4996b2ebfa86
KT <KT@T14.local>
2019-03-16 04:51:10 +0900

    update pages

--- src/Content/About/About.tsx
+++ src/Content/About/About.tsx
@@ -1,17 +1,33 @@
 import * as React from "react";
-import { Twitter } from "./Twitter";
-import { GitHub } from "./GitHub";
 
 // About
 export class About extends React.Component {
   // render
   render() {
+    const twitter = {
+      href: "https://twitter.com/TSJKZSH",
+      src: "./../../../img/Twitter_Logo_Blue.png",
+      alt: "twitter"
+    };
+    const github = {
+      href: "https://github.com/2G14",
+      src: "./../../../img/GitHub-Mark-Light-120px-plus.png",
+      alt: "github"
+    };
+    const links = [twitter, github];
     return (
       <div>
         <h1>About</h1>
         <ul>
-          <li><Twitter /></li>
-          <li><GitHub /></li>
+          {links.map((link, i) => {
+            return (
+              <li key={i}>
+                <a href={link.href} target="_blank">
+                  <img src={link.src} alt={link.alt} width="200" height="200" />
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     );
