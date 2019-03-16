d94185767bac910c2e23088e32dabb8634426a14
KT <KT@T14.local>
2019-03-16 19:29:54 +0900

    update server

--- server.js
+++ server.js
@@ -1,47 +1,64 @@
 "use strict";
 
+// core module
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
 
+// mime tyeps
 const mimeTypes = require("./serverContent/mimetypes");
+// white list
 const whitelist = require("./serverContent/whitelist");
 
+// server
 const server = http.createServer();
-server.on("request", function(request, response) {
+server.on("request", onRequest);
+server.listen(8080); // server listen 8080 port
+
+console.log("Server runnning at localhost:8080");
+
+/* -------------------------------------------------------*/
+
+// callback function : on request
+function onRequest(request, response) {
   console.log(request.url);
 
+  // request file path
   let pathName = url.parse(decodeURI(request.url)).pathname;
   pathName = path.normalize(pathName);
-  pathName = (pathName === "/") ? "/index.html" : pathName;
-  
+  pathName = pathName === "/" ? "/index.html" : pathName;
+
+  // check white list
   if (whitelist.indexOf(pathName) === -1) {
+    // response
     response.writeHead(404);
-    response.end("page not found");
+    response.end("file not found");
     return;
   }
-  
+
+  // file full path
   const fullPath = path.join(__dirname, pathName);
-  
+
+  // response
   response.writeHead(200, {
     "Content-Type": mimeTypes[path.extname(fullPath)]
   });
-  let file = null;
+  // callback function : file upload
+  function fileUpload(err, data) {
+    if (err) throw err;
+    response.write(data);
+    response.end();
+  }
   switch (path.extname(fullPath)) {
     case ".html":
     case ".css":
     case ".js":
-      file = fs.readFileSync(fullPath, "UTF-8");
-      response.end(file);
+      fs.readFile(fullPath, "UTF-8", fileUpload); // encode UTF-8
       break;
     case ".ico":
     case ".png":
-      file = fs.readFileSync(fullPath, "binary");
-      response.end(file, "binary");
+      fs.readFile(fullPath, fileUpload); // encode binary
       break;
   }
-});
-server.listen(8080);
-
-console.log("Server runnning at localhost:8080");
+}
